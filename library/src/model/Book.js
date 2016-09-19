f84d2ce8f781cdea9278f6ca03b142244747cd23
ryanbyoung <ryanbyoung@users.noreply.github.com>
2016-09-19 10:55:11 -0700

    Update Book.js

--- library/src/model/Book.js
+++ library/src/model/Book.js
@@ -60,7 +60,7 @@ Book.update = function (slots) {
 };
 //  Delete a book row from storage
 Book.destroy = function (isbn) {
-  console.log(isbn);
+  alert(isbn + " from Book.js");
   if (Book.instances[isbn]) {
     document.getElementById("confirm").innerHTML = "Book " + isbn + " deleted";
     delete Book.instances[isbn];
