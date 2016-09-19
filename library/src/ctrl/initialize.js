78f51375010d3900e9272e858e7908e368ce1f60
ryanbyoung <ryanbyoung@users.noreply.github.com>
2016-09-19 11:49:22 -0700

    Update initialize.js

--- library/src/ctrl/initialize.js
+++ library/src/ctrl/initialize.js
@@ -1,6 +1,2 @@
-/**
- * @fileOverview  Defining the main namespace ("public library") and its MVC subnamespaces
- * @author Gerd Wagner
- */
 // main namespace pl = "public library"
 var pl = { model:{}, view:{}, ctrl:{} };
