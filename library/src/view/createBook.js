c427a72a002fc117a6bab496d312edb74afca52e
ryanbyoung <ryanbyoung@users.noreply.github.com>
2019-06-08 19:36:49 -0400

    Update createBook.js

--- library/src/view/createBook.js
+++ library/src/view/createBook.js
@@ -5,26 +5,27 @@ pl.view.createBook = {
     Book.loadAll();
     // Set an event handler for the save/submit button
     saveButton.addEventListener("click", 
-        pl.view.createBook.handleSaveButtonClickEvent);
+      pl.view.createBook.handleSaveButtonClickEvent);
     window.addEventListener("beforeunload", function () {
-        Book.saveAll(); 
+      Book.saveAll(); 
     });
   },
   // save user input data
   handleSaveButtonClickEvent: function () {
     var formEl = document.forms['Book'];
     if (formEl.isbn.value != "") {
-        var slots = { 
-            isbn: formEl.isbn.value, 
-            title: formEl.title.value, 
-            year: formEl.year.value
-        };
-        Book.create(slots);
-        formEl.reset();
-        document.getElementByID("required").innerText = "";
+      var slots = { 
+        isbn: formEl.isbn.value, 
+        title: formEl.title.value, 
+        year: formEl.year.value
+      };
+      Book.create(slots);
+      formEl.reset();
+      document.getElementByID("required").innerText = "";
     }
     else {
-        formEl.isbn.focus();
-        document.getElementByID("required").innerText = "ISBN is required";
+      formEl.isbn.focus();
+      document.getElementByID("required").innerText = "ISBN is required";
+    }
   }
 };
