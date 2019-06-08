a1ce7c25c0c1055e6fe1eddf9ba6d93775584877
ryanbyoung <ryanbyoung@users.noreply.github.com>
2019-06-08 17:09:04 -0400

    Update createBook.js

--- library/src/view/createBook.js
+++ library/src/view/createBook.js
@@ -6,7 +6,7 @@ pl.view.createBook = {
     // Set an event handler for the save/submit button
     saveButton.addEventListener("click", 
         pl.view.createBook.handleSaveButtonClickEvent);
-    window.addEventListener("unload", function () {
+    document.addEventListener("unload", function () {
         Book.saveAll(); 
     });
   },
