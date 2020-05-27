98d272a43a3bd75d6cd2a16db09aa58f047c8468
ryanbyoung <ryanbyoung@users.noreply.github.com>
2020-05-26 21:12:38 -0400

    Update sw.js

--- vids/sw.js
+++ vids/sw.js
@@ -3,8 +3,8 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open('v1').then((cache) => {
       return cache.addAll([
-        '/',
-        'index.html'
+        '/vids/',
+        '/vids/index.html'
       ]);
     })
   );
