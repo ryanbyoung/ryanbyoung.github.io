2ee5c2e6bdec2f79fe1fa57d7c06ebedca90a8e9
ryanbyoung <ryanbyoung@users.noreply.github.com>
2020-05-26 21:03:08 -0400

    Update sw.js

--- vids/sw.js
+++ vids/sw.js
@@ -1,3 +1,16 @@
-self.addEventListener('install', (e) => {
+self.addEventListener('install', (event) => {
   console.log('[Service Worker] Install');
+  event.waitUntil(
+    caches.open('v1').then((cache) => {
+      return cache.addAll([
+        'index.html',
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+  );
 });
