555bb34a74d3c484a67e35bc5b7b936e64aa6d25
ryanbyoung <ryanbyoung@users.noreply.github.com>
2020-06-10 23:23:46 -0400

    Update sw.js

--- vids/sw.js
+++ vids/sw.js
@@ -1,18 +1,79 @@
-self.addEventListener('install', (event) => {
-  console.log('[Service Worker] Install');
+/*
+ Copyright 2014 Google Inc. All Rights Reserved.
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+// While overkill for this specific sample in which there is only one cache,
+// this is one best practice that can be followed in general to keep track of
+// multiple caches used by a given service worker, and keep them all versioned.
+// It maps a shorthand identifier for a cache to a specific, versioned cache name.
+
+// Note that since global state is discarded in between service worker restarts, these
+// variables will be reinitialized each time the service worker handles an event, and you
+// should not attempt to change their values inside an event handler. (Treat them as constants.)
+
+// If at any point you want to force pages that use this service worker to start using a fresh
+// cache, then increment the CACHE_VERSION value. It will kick off the service worker update
+// flow and the old cache(s) will be purged as part of the activate event handler when the
+// updated service worker is activated.
+var CACHE_VERSION = 1;
+var CURRENT_CACHES = {
+  prefetch: 'prefetch-cache-v' + CACHE_VERSION
+};
+
+self.addEventListener('install', function(event) {
+  var urlsToPrefetch = [
+    '/vids/',
+    '/vids/index.html',
+    '/vids/channels/',
+    '/vids/channels/dane-reynolds/'
+    // Add videos here
+  ];
+
+  // All of these logging statements should be visible via the "Inspect" interface
+  // for the relevant SW accessed via chrome://serviceworker-internals
+  console.log('Handling install event. Resources to prefetch:', urlsToPrefetch);
+
+  self.skipWaiting();
+
   event.waitUntil(
-    caches.open('v1').then((cache) => {
-      return cache.addAll([
-        '/vids/',
-        '/vids/index.html',
-        '/vids/channels/',
-        '/vids/channels/dane-reynolds/'
-      ]);
+    caches.open(CURRENT_CACHES.prefetch).then(function(cache) {
+      return cache.addAll(urlsToPrefetch);
     })
   );
 });
-//Using this fetch listener for videos
-//This code is based on  https://googlechrome.github.io/samples/service-worker/prefetch-video/ 
+
+self.addEventListener('activate', function(event) {
+  // Delete all caches that aren't named in CURRENT_CACHES.
+  // While there is only one cache in this example, the same logic will handle the case where
+  // there are multiple versioned caches.
+  var expectedCacheNames = Object.keys(CURRENT_CACHES).map(function(key) {
+    return CURRENT_CACHES[key];
+  });
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (expectedCacheNames.indexOf(cacheName) === -1) {
+            // If this cache name isn't present in the array of "expected" cache names, then delete it.
+            console.log('Deleting out of date cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+        );
+    })
+    );
+});
+
 self.addEventListener('fetch', function(event) {
 
   headersLog = [];
@@ -99,27 +160,3 @@ self.addEventListener('fetch', function(event) {
     );
   }
 });
-/* From Mozilla
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((resp) => {
-      return resp || fetch(event.request).then((response) => {
-        let responseClone = response.clone();
-        caches.open('v1').then((cache) => {
-          cache.put(event.request, responseClone);
-        });
-        return response;
-      });
-    }).catch(() => {
-      return ('<p>Sorry, something has gone wrong. Return <a href="/vids/">home</a>.</p>');
-    })
-  );
-});
-*/
-/* Old fetch event
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
-*/
