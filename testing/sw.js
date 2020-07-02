8b037c3947fac465f95780c4fd4c3aa4df279340
ryanbyoung <ryanbyoung@users.noreply.github.com>
2020-07-01 20:53:23 -0400

    Create sw.js

---
+++ testing/sw.js
@@ -0,0 +1,14 @@
+self.addEventListener('install', function(event) {
+    console.log('SW INSTALLING');
+    const installCompleted = Promise.resolve()
+      .then(() => console.log('SW INSTALLED'));
+    event.waitUntil(installCompleted);
+  }
+});
+
+self.addEventListener('activate', event => {
+  console.log('SW ACTIVATING');
+  const activationCompleted = Promise.resolve()
+    .then((activationCompleted) => console.log('SW ACTIVATED'));
+  event.waitUntil(activationCompleted);
+});
