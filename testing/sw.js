53a82bed67b3e5e3f3bd6f995bb84b2a61a5f285
ryanbyoung <ryanbyoung@users.noreply.github.com>
2020-07-06 19:37:32 -0400

    Update sw.js

--- testing/sw.js
+++ testing/sw.js
@@ -11,3 +11,27 @@ self.addEventListener('activate', event => {
     .then((activationCompleted) => console.log('SW ACTIVATED'));
   event.waitUntil(activationCompleted);
 });
+
+self.addEventListener('fetch', function(event) {
+  // Get current path
+  var requestUrl = new URL(event.request.url);
+
+  // Save all resources on origin path only
+  if (requestUrl.origin === location.origin) {
+      event.respondWith(
+        // Open the cache created when install
+        caches.open(cacheName).then(function(cache) {
+          // Go to the network to ask for that resource
+          return fetch(event.request).then(function(networkResponse) {
+            // Add a copy of the response to the cache
+            cache.put(event.request, networkResponse.clone());
+            // Respond with it
+            return networkResponse;
+          }).catch(function() {
+            // If no internet connection, try to match request
+            // to some of our cached resources
+            return cache.match(event.request);
+          })
+        })
+      );
+  }
